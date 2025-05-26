176b2ad07d5ac5b35006ccd1109371d9b130b831
caobaoqi1024 <caobaoqi1024@gmail.com>
2025-05-26 11:27:05 +0800

    ✨ feat(src): support for .h

--- src/0_hello_world.cpp
+++ src/0_hello_world.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "../include/hello_world.h"
 using namespace std;
 
+void say_hello(const std::string& username) {
+    cout << "Hello, " << username << "!" << endl;
+}
+
 int main() {
-    cout << "0-Hello, world" << endl;
+    say_hello("caobaoqi1024");
     return 0;
 } 
