afd366c07f00428ed507ace7dea1113ae70d5985
caobaoqi1024 <caobaoqi1024@gmail.com>
2025-05-26 11:56:33 +0800

    ✨ feat(core): 1-两数之和

--- src/1_two_num_sum.cpp
+++ src/1_two_num_sum.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+#include <vector>
+#include <map>
 using namespace std;
 
-int main() {
-    cout << "1-Hello, world" << endl;
-    return 0;
-} 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> a;//建立 hash 表存放数组元素
+        vector<int> b(2,-1);//存放结果
+        for(int i=0;i<nums.size();i++)
+            a.insert(map<int,int>::value_type(nums[i],i));
+        for(int i=0;i<nums.size();i++)
+        {
+            if(a.count(target-nums[i])>0&&(a[target-nums[i]]!=i))
+            //判断是否找到目标元素且目标元素不能是本身
+            {
+                b[0]=i;
+                b[1]=a[target-nums[i]];
+                break;
+            }
+        }
+        return b;
+    };
+};
+// @lc code=end
+
